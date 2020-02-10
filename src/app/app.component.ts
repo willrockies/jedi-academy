85eaf7e3d20c6a16867b83439bf030b46c29d367
Wilson Alves da Silva <w.alves.da.silva@avanade.com>
2020-02-09 21:52:05 -0300

    basicão de angular-cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Student } from "./student/student.model";
 
 @Component({
-  selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "jad-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  students: Student[] = [
+    { name: "Luke", isJedi: true, temple: "Coruscant" },
+    { name: "Han Solo", isJedi: false },
+    { name: "Leia", isJedi: false }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { StudentComponent } from "./student/student.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, StudentComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
